2a833b2be9dd0dfab995b9cb20a7e38dd3a41567
Anton Trofimenko <r37r0m0d3l@protonmail.com>
2021-03-19 11:04:01 +0200

    feat(deps): update dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import rollupPluginTerser from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser";
 
 const INPUT_NAME = "index.mjs";
 const OUTPUT_NAME = "fallback-local-storage";
@@ -32,13 +32,13 @@ export default {
     babel({ babelrc: true }),
     resolve(),
     commonjs(),
-    rollupPluginTerser.terser({
+    terser({
       keep_classnames: true,
       keep_fnames: true,
       output: {
         comments: false,
       },
-      sourcemap: true,
+      // sourcemap: true,
       warnings: true,
     }),
   ],
